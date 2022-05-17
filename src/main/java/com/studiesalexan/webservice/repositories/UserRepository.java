486a897e2bfea2384a0d50c2ade00809de9f066b
Alexan Lima <alexanlima1.0@gmail.com>
2022-05-17 14:52:31 -0300

    Service layer e component registration

--- src/main/java/com/studiesalexan/webservice/repositories/UserRepository.java
+++ src/main/java/com/studiesalexan/webservice/repositories/UserRepository.java
@@ -3,6 +3,9 @@ package com.studiesalexan.webservice.repositories;
 import com.studiesalexan.webservice.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findAllById(Long id);
 }

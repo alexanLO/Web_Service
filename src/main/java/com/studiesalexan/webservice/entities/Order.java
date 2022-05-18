947bc2d64494797dbcac58869f4d780f8f33da54
Alexan Lima <alexanlima1.0@gmail.com>
2022-05-18 09:52:36 -0300

    OrderStatus enum

--- src/main/java/com/studiesalexan/webservice/entities/Order.java
+++ src/main/java/com/studiesalexan/webservice/entities/Order.java
@@ -16,6 +16,8 @@ public class Order {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -23,9 +25,10 @@ public class Order {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -48,9 +51,18 @@ public class Order {
     public User getClient() {
         return client;
     }
-
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override

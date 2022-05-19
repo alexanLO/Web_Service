740aa1e4f32bf60941aa285d753d2f9f7295ca4f
Alexan Lima <alexanlima1.0@gmail.com>
2022-05-19 14:49:31 -0300

    Exception handling - update

--- src/main/java/com/studiesalexan/webservice/services/UserService.java
+++ src/main/java/com/studiesalexan/webservice/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,10 +43,16 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
-    }
+           try {
+               User entity = userRepository.getById(id);
+               updateData(entity, obj);
+               return userRepository.save(entity);
+           }catch (EntityNotFoundException e){
+               throw new ResourceNotFoundException(id);
+           }
+           }
+
+
 
     private void updateData(User entity, User obj) {
         entity.setName(obj.getName());
